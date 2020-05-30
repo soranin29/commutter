f1d08b7fd564859b140c5896f5688ce54238b154
soranin29 <cyndiiichannel@gmail.com>
2020-05-30 17:28:30 +0900

    railsを導入した

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def name:string
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def me
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'sessions/create'
+
+  get 'sessions/destory'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
