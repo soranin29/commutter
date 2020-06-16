a78fc04c9ae49437b5f78d8ce8e8b0ff92f0525a
soranin29 <cyndiiichannel@gmail.com>
2020-06-17 02:25:48 +0900

    投稿機能の雛形を作成した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def show
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -50,10 +50,10 @@ class PostsController < ApplicationController
 
   private
     def set_post
-      @post = Post.find(params[:id])
+      @post = current_user.posts.find(params[:id])
     end
 
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:user_id, :body)
     end
 end
