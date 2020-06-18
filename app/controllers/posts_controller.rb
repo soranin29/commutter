5e6971ab7bc1ba9169941c89b4b66f5a30a2fd16
soranin29 <cyndiiichannel@gmail.com>
2020-06-18 19:00:19 +0900

    パラメーターを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,6 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.new(post_params)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
